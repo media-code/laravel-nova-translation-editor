f46a0f423d761081ba997027370378da4b4d0b06
Cas Berkman <cas@mediacode.nl>
2020-02-17 15:33:55 +0100

    added a translatables key to the config file

--- config/trans_editor.php
+++ config/trans_editor.php
@@ -1,10 +1,13 @@
 <?php
 return [
+    // Translatables are the translation files to be translated
+    'translatables' => [
+
+    ],
 
     // These are the two standard supported locales, add, remove or modify them if you want
     'supported-locales' => [
         'en' => 'English',
-        'de' => 'Deutsch',
-        'nl' => 'Nederlands',
+        'de' => 'German',
     ]
 ];
